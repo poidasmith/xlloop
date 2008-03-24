d66af17f1e24cef06dafe085492e37447558f8d6
poida_smith <poida_smith>
2008-03-24 19:35:51 +0000

    *** empty log message ***

--- org.boris.xlloop/java/test/org/boris/functionserver/ServerTest.java
+++ org.boris.xlloop/java/test/org/boris/functionserver/ServerTest.java
@@ -2,12 +2,11 @@ package org.boris.functionserver;
 
 import org.boris.functionserver.reflect.ReflectFunctionHandler;
 
-
-public class ServerTest {
+public class ServerTest 
+{
     public static void main(String[] args) throws Exception {
         FunctionServer fs = new FunctionServer(5600);
         ReflectFunctionHandler rfh = new ReflectFunctionHandler();
-        rfh.addMethods("CSV.", CSV.class);
         rfh.addMethods("Math.", Math.class);
         fs.setFunctionHandler(rfh);
         fs.run();
