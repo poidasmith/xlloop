77d3e9e64ad102657f3fb8510f5e028bf3c2b621
poida_smith <poida_smith>
2008-04-05 11:07:50 +0000

    *** empty log message ***

--- org.boris.xlloop/java/test/org/boris/functionserver/ServerTest1.java
+++ org.boris.xlloop/java/test/org/boris/functionserver/ServerTest1.java
@@ -5,6 +5,10 @@ import java.io.File;
 import org.boris.functionserver.reflect.ReflectFunctionHandler;
 import org.boris.functionserver.script.ScriptRepository;
 import org.boris.functionserver.util.CompositeFunctionHandler;
+import org.boris.functionserver.util.DebugFunctionHandler;
+import org.boris.variantcodec.VTCollection;
+import org.boris.variantcodec.VTStruct;
+import org.boris.variantcodec.Variant;
 
 public class ServerTest1 {
     public static void main(String[] args) throws Exception {
@@ -17,7 +21,19 @@ public class ServerTest1 {
         CompositeFunctionHandler cfh = new CompositeFunctionHandler();
         cfh.add(rfh);
         cfh.add(srep);
-        fs.setFunctionHandler(cfh);
+        fs.setFunctionHandler(new DebugFunctionHandler(cfh));
+        RequestMap rh = new RequestMap();
+        rh.add("GetFunctions", new Request() {
+            public Variant execute(VTStruct args) throws RequestException {
+                VTCollection functions = new VTCollection();
+                VTStruct random = new VTStruct();
+                random.add("functionName", "Math.random");
+                random.add("functionText", "Math_random");
+                random.add("isVolatile", true);
+                functions.add(random);
+                return functions;
+            }});
+        fs.setRequestHandler(rh);
         fs.run();
     }
 }
