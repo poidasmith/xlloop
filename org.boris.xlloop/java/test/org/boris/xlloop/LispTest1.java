c46f9c6074ff18204c211453de1291635a465868
poida_smith <poida_smith>
2008-04-14 16:22:59 +0000

    *** empty log message ***

---
+++ org.boris.xlloop/java/test/org/boris/xlloop/LispTest1.java
@@ -0,0 +1,34 @@
+package org.boris.xlloop;
+
+import java.io.File;
+
+import org.boris.xlloop.script.LispFunctionHandler;
+import org.boris.xlloop.util.DebugFunctionHandler;
+import org.boris.xlloop.util.DebugRequestHandler;
+import org.boris.xlloop.util.FunctionInformationRequestHandler;
+
+public class LispTest1 {
+    public static void main(String[] args) throws Exception {
+        // Create a new function server on default port
+        FunctionServer fs = new FunctionServer();
+        
+        // Create our lisp function handler
+        LispFunctionHandler lfh = new LispFunctionHandler();
+
+        // Evaluate any lisp files in this directory (and sub-dirs)
+        lfh.eval(new File("functions"), true); 
+        
+        // Expose a function called "Eval" for the lisp handler
+        FunctionInformationRequestHandler firh = new FunctionInformationRequestHandler();
+        firh.add(lfh.getInformation()); 
+
+        // Set the function handler
+        fs.setFunctionHandler(new DebugFunctionHandler(lfh));
+        
+        // Set the request handler (for the function information)
+        fs.setRequestHandler(new DebugRequestHandler(firh));
+        
+        // Run the engine
+        fs.run();
+    }
+}
