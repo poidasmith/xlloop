2b05aa201255ab5e00b347cb0c6e0b9ebb9e46b7
poida_smith <poida_smith>
2008-04-15 21:53:24 +0000

    *** empty log message ***

--- org.boris.xlloop/java/src/org/boris/xlloop/util/CSV.java
+++ org.boris.xlloop/java/src/org/boris/xlloop/util/CSV.java
@@ -9,6 +9,13 @@
  *******************************************************************************/
 package org.boris.xlloop.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class CSV
 {
     public static String toCSV(String[] arr) {
@@ -20,4 +27,59 @@ public class CSV
         }
         return sb.toString();
     }
+    
+    /**
+     * Parse a line.
+     *
+     * @param line.
+     *
+     * @return String[].
+     */
+    public static String[] parseLine(String line, char delim, boolean hasQuotes) {
+        ArrayList items = new ArrayList();
+        StringBuilder sb = new StringBuilder();
+        boolean inQuote = false;
+        int length = line.length();
+
+        for (int i = 0; i < length; i++) {
+            char c = line.charAt(i);
+
+            if ((c == delim) && !inQuote) {
+                items.add(sb.toString());
+                sb.setLength(0);
+            } else if (hasQuotes && (c == '\"')) {
+                inQuote = !inQuote;
+            } else {
+                sb.append(c);
+            }
+        }
+
+        items.add(sb.toString());
+
+        return (String[]) items.toArray(new String[0]);
+    }
+
+    /**
+     * Try to open the given url as a file or url.
+     *
+     * @param url.
+     *
+     * @return BufferedReader.
+     */
+    public static BufferedReader openUrl(String url) throws Exception {
+        // Try a url first
+        try {
+            URL u = new URL(url);
+
+            return new BufferedReader(new InputStreamReader(u.openStream()));
+        } catch (Exception ex) {
+            File f = new File(url);
+
+            if (f.exists()) {
+                return new BufferedReader(new FileReader(f));
+            }
+        }
+
+        return null;
+    }    
 }
