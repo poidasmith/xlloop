e76e2fbfd1af8e9f06357e269e1af54db606d646
poida_smith <poida_smith>
2008-04-23 09:41:07 +0000

    *** empty log message ***

--- org.boris.xlloop/erlang-server/server_example.erl
+++ org.boris.xlloop/erlang-server/server_example.erl
@@ -1,16 +1,18 @@
 -module(server_example).
 -export([start/0, stop/0, request/2, function/2]).
 -behaviour(xlloop_server).
+-define(PORT, 5454).
 
 start() ->
-	register(sep, xlloop_server:start(5454, server_example)).
+	Pid = xlloop_server:start(?PORT, ?MODULE),
+	register(server_example_pid, Pid).
 	
 stop() ->
-	sep ! stop.
+	server_example_pid ! stop.
 
 request(_Name, _Args) ->
 	{collection, []}.
 	
-function(Name, _Args) ->
+function(_Name, _Args) ->
 	{string, "Hello World!"}.
 	
--- org.boris.xlloop/erlang-server/xlloop_server.erl
+++ org.boris.xlloop/erlang-server/xlloop_server.erl
@@ -1,41 +1,61 @@
 -module(xlloop_server).
--export([start/1,stop/0]).
+-export([start/2, behaviour_info/1]).
 
 -define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}]).
+-define(TIME_OUT, 1000).
+-define(REPLY_OK, "Ok").
+-define(REPLY_ERROR, "Error").
+-define(REQ_TYPE_GENERIC, 0).
+-define(REQ_TYPE_FUNCTION, 1).
 
-stop() ->
-	get(server_pid) ! stopped.
+start(Port, Module) when is_integer(Port) ->
+	spawn(fun() -> listen(Port, Module) end). 
 	
-start(Port) when is_integer(Port) ->
-	put(server_pid, spawn(fun() -> listen(Port) end)).
+behaviour_info(callbacks) ->
+	[{request, 2}, {function, 2}].
 	
-listen(Port) ->
-	put(state, running),
+listen(Port, Module) ->
 	{ok, Socket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
-	accept(Socket).
+	accept(Socket, Module).
 	
-accept(ServerSocket) ->
-	{ok, Socket} = gen_tcp:accept(ServerSocket),
-	receive
-		stopped -> gen_tcp:close(Socket), {ok, stopped}
-	after 
-		0 -> spawn(fun() -> handle(Socket) end), accept(ServerSocket)
+accept(ServerSocket, Module) ->
+	{A, B} = gen_tcp:accept(ServerSocket, ?TIME_OUT),
+	case {A, B} of
+		{ok, Socket} -> 
+			spawn(fun() -> handle(Socket, Module) end),
+			accept(ServerSocket, Module);
+		{error, timeout} ->
+			receive
+				stop -> {ok, stopped}
+			after 
+				0 -> accept(ServerSocket, Module)
+			end
 	end.
 	
-handle(Socket) ->
+handle(Socket, Module) ->
 	case gen_tcp:recv(Socket, 0) of
-		{ok, Data} -> data(Socket, Data);
+		{ok, Data} -> 
+			data(Socket, Data, Module),
+			handle(Socket, Module);
 		{error, closed} -> {error}
 	end.
 	
-data(Socket, Data) ->
-	{Variant, _Rest} = variant_codec:decode(Data),
-	Res = function(Variant),
-	Type = variant_codec:encode({long, 1}),
-	gen_tcp:send(Socket, Type),
-	Bin = variant_codec:encode(Res),
-	gen_tcp:send(Socket, Bin),
-	handle(Socket).
+data(Socket, Data, Module) ->
+	{Type, Rest} = variant_codec:decode(Data),
+	{Name, Rest2} = variant_codec:decode(Rest),
+	{Args, _Rest3} = variant_codec:decode(Rest2),
+	case Type of
+		{long, ?REQ_TYPE_GENERIC} -> request(Socket, Name, Args, Module);
+		{long, ?REQ_TYPE_FUNCTION} -> function(Socket, Name, Args, Module)
+	end.
 		
-function(_Variant) ->
-	{string, "Hello World!"}.
+function(Socket, Name, Args, Module) ->
+	reply(Socket, ?REPLY_OK, Module:function(Name, Args)).
+	
+request(Socket, Name, Args, Module) ->
+	reply(Socket, ?REPLY_OK, Module:request(Name, Args)).
+	
+reply(Socket, Type, Variant) ->
+	gen_tcp:send(Socket, variant_codec:encode({string, Type})),
+	gen_tcp:send(Socket, variant_codec:encode(Variant)).
+	
