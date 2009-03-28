7c62cc2184e9daf4b4b188fdc3d083a14d877d33
poida_smith <poida_smith>
2009-03-28 01:55:19 +0000

    *** empty log message ***

---
+++ org.boris.xlloop/java/src/org/boris/xlloop/util/Day.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     Peter Smith
+ *******************************************************************************/
+package org.boris.xlloop.util;
+
+public class Day
+{
+    private int year;
+    private int month;
+    private int day;
+
+    public Day(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+}
