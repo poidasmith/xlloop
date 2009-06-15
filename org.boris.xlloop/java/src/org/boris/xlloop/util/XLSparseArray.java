f935a8a3a928e170a1dbaa1acf3fc1c242c1611b
poida_smith <poida_smith>
2009-06-15 06:23:46 +0000

    *** empty log message ***

---
+++ org.boris.xlloop/java/src/org/boris/xlloop/util/XLSparseArray.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     Peter Smith
+ *******************************************************************************/
+package org.boris.xlloop.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.boris.xlloop.xloper.XLArray;
+import org.boris.xlloop.xloper.XLBool;
+import org.boris.xlloop.xloper.XLInt;
+import org.boris.xlloop.xloper.XLNil;
+import org.boris.xlloop.xloper.XLNum;
+import org.boris.xlloop.xloper.XLString;
+import org.boris.xlloop.xloper.XLoper;
+
+public class XLSparseArray
+{
+    private int minRow = Integer.MAX_VALUE;
+    private int minCol = Integer.MAX_VALUE;
+    private int maxRow = 0;
+    private int maxCol = 0;
+    private Map<ArrayRef, XLoper> values = new HashMap();
+
+    public void set(int row, int column, String value) {
+        set(row, column, new XLString(value));
+    }
+
+    public void set(int row, int column, boolean value) {
+        set(row, column, value ? XLBool.TRUE : XLBool.FALSE);
+    }
+
+    public void set(int row, int column, double value) {
+        set(row, column, new XLNum(value));
+    }
+
+    public void set(int row, int column, int value) {
+        set(row, column, new XLInt(value));
+    }
+
+    public void set(int row, int column, XLoper value) {
+        if (value == null)
+            value = XLNil.NIL;
+        if (minRow > row)
+            minRow = row;
+        if (maxRow < row)
+            maxRow = row;
+        if (minCol > column)
+            minCol = column;
+        if (maxCol < column)
+            maxCol = column;
+        values.put(new ArrayRef(row, column), value);
+    }
+
+    public XLoper toXLoper() {
+        XLArray a = new XLArray(maxRow - minRow + 1, maxCol - minCol + 1);
+        for (ArrayRef r : values.keySet()) {
+            a.set(r.row - minRow, r.column - minCol, values.get(r));
+        }
+        return a;
+    }
+
+    private class ArrayRef
+    {
+        public ArrayRef(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        public int row;
+        public int column;
+
+        public boolean equals(Object obj) {
+            return ((ArrayRef) obj).row == row && ((ArrayRef) obj).column == column;
+        }
+
+        public int hashCode() {
+            return row ^ column;
+        }
+    }
+}
