5b2c566e78779854c221ebacb93e5fc4cab342d5
poida_smith <poida_smith>
2009-06-19 18:57:34 +0000

    *** empty log message ***

--- org.boris.xlloop/test/make-release-xll.cmd
+++ org.boris.xlloop/test/make-release-xll.cmd
@@ -1,4 +1,4 @@
-set VER=0.2we.0
+set VER=0.1.0
 copy F:\eclipse\workspace\WinRun4J\build\XLLoop-Release\XLLoop.xll F:\Development\Releases\xlloop.appspot.com-%VER%.xll
 "F:\eclipse\workspace\WinRun4J\build\ResourceEditor-Release - Console\ResourceEditor.exe" /N F:\Development\Releases\xlloop.appspot.com-%VER%.xll F:\eclipse\workspace\org.boris.xlloop\test\embed.ini
 pause
