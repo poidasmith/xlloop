cad1ef54908592f5b189dd4eb8b35bcc7061313c
poida_smith <poida_smith>
2011-02-20 03:53:41 +0000

    *** empty log message ***

--- org.boris.xlloop/java/test/org/boris/xlloop/GenDef.java
+++ org.boris.xlloop/java/test/org/boris/xlloop/GenDef.java
@@ -12,9 +12,18 @@ package org.boris.xlloop;
 public class GenDef
 {
     public static void main(String[] args) throws Exception {
+        int index = 8;
+        for (int i = 0; i < 20; i++) {
+            System.out.println("\tFSExecute" + i + " @" + (index++));
+        }
+        for (int i = 0; i < 20; i++) {
+            System.out.println("\tFSExecuteVolatile" + i + " @" + (index++));
+        }
         for (int i = 0; i < 512; i++) {
-            // System.out.println("\tFSC" + i + " @" + (522 + i));
-            System.out.println("DECLARE_EXCEL_COMMAND(" + i + ")");
+            System.out.println("\tFS" + i + " @" + (index++));
+        }
+        for (int i = 0; i < 512; i++) {
+            System.out.println("\tFSC" + i + " @" + (index++));
         }
     }
 }
