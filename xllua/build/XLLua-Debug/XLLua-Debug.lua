1f347b0af32c461c1200e4778f4bfb6383321ec9
poidasmith <poida.smith@gmail.com>
2012-08-05 16:53:18 +0800

    load all addins

--- xllua/build/XLLua-Debug/XLLua-Debug.lua
+++ xllua/build/XLLua-Debug/XLLua-Debug.lua
@@ -1,72 +1,12 @@
---[[ ***************************************************************************
-* 
-* This program and the accompanying materials
-* are made available under the terms of the Common Public License v1.0
-* which accompanies this distribution, and is available at 
-* http://www.eclipse.org/legal/cpl-v10.html
-* 
-* Contributors:
-*     Peter Smith
-******************************************************************************]]
 
-package.path  = package.path .. ";F:/Development/Lua/5.1/lua/?.lua"
-package.cpath = package.cpath .. ";F:/Development/Lua/5.1/clibs/?.dll"
+package.path = "../src/?.lua;" .. package.path
 
-xllua.debug_printf( "%s\n%s\n", package.path, package.cpath );
-
-local os    = require 'os'
-local redis = require 'redis'
-
-
--- named connections
-
-local conns = {}
-
-local function connection(args)
-	name, host, port = unpack(args)
-	local c = conns[name]
-	if c ~= nil then
-		c.c:quit()
+local f, error = loadfile( "addins.lua" )
+if f ~= nil then
+	local res, err = pcall( f )
+	if not res then
+		xllua.debug_printf( "error: %s\n", stringit( err ) )
 	end
-		
-	host = host or "localhost"
-	port = port or 6379
-	
-	conns[name] = {
-		host = host,
-		port = port,
-		c    = redis.connect(host, port),
-	}
-		
-	return string.format("%s@%s:%d (%f)", name, host, port, os.clock())
-end	
-
--- redis command wrapper
-
-local function stdf(name)
-	return function(args)
-		local svr = table.remove(args, 1)
-		local c = conns[svr];
-		if c == nil then
-			return string.format("#Unknown server: %s", xllua.stringit( name ) )
-		end 
-		local f = c.c[name]
-		return f(c.c, args)
-	end	 
-end
-
--- our redis commands, wrapped in a connection lookup function 
-
-fns = {}
-
-for k, v in pairs(redis.commands) do
-	fns["rds." .. k] = stdf(k)
-end
-
-fns["rds.connect"] = connection
-
--- register all the redis commands with Excel
-
-xllua.reg_funs( fns ) 
-
-xllua.debug_printf( "xlredis...\n" )
+else
+	xllua.debug_printf( "error: %s\n", error )
+end 
